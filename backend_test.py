6eb061cf59048a262d61819e6ba9c37f3dedd945
Emergent Agent <emergent@agent.com>
2026-02-15 01:16:28 +0000

    auto-commit for 9c055928-d21f-4001-8811-cea210d76cb6

--- backend_test.py
+++ backend_test.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 class MediaBasherAPITester:
-    def __init__(self, base_url="https://dashbash.preview.emergentagent.com"):
+    def __init__(self, base_url="https://mediabasher.preview.emergentagent.com"):
         self.base_url = base_url
         self.token = None
         self.tests_run = 0
